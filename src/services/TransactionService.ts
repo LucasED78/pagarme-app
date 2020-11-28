0fcab05304525ab2e75f79c3cc192eaf33bacaf0
Lucas Eduardo <lucased78@hotmail.com>
2020-11-28 20:28:12 -0300

    feat: added transaction service

---
+++ src/services/TransactionService.ts
@@ -0,0 +1,20 @@
+import { ITransactionsFilter, TransactionResponse } from '../types/transaction';
+import ITransactionService from '../types/services';
+
+class TransactionService implements ITransactionService {
+  filter = (filter: ITransactionsFilter, transactions: TransactionResponse[]): TransactionResponse[] => {
+    const filteredTransactions = transactions.filter(transaction => {      
+      if (filter?.status && filter?.paymentMethod) {
+        return transaction.status === filter.status && transaction.payment_method === filter.paymentMethod;
+      } else if (filter?.status) {
+        return transaction.status === filter.status;
+      } else {
+        return transaction.payment_method === filter.paymentMethod;
+      }
+    })
+
+    return filteredTransactions;
+  }
+}
+
+export default TransactionService;
